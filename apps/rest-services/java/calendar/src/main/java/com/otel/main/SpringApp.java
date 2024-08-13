4634891552f185dea1a60bfb81edbcd56a2fa57d
Eugene Kirillov <eugene.kirillov@datadoghq.com>
2024-08-13 13:50:29 -0700

    Calendar app: fix helm instructions in the README

--- apps/rest-services/java/calendar/src/main/java/com/otel/main/SpringApp.java
+++ apps/rest-services/java/calendar/src/main/java/com/otel/main/SpringApp.java
@@ -3,30 +3,31 @@ Unless explicitly stated otherwise all files in this repository are licensed
 under the Apache 2.0 License.
 
 This product includes software developed at Datadog (https://www.datadoghq.com/)
-Copyright 2023 Datadog, Inc.
- */
+Copyright 2024 Datadog, Inc.
+*/
 package com.otel.main;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
-import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+
 @SpringBootApplication(scanBasePackages = "com.otel")
 public class SpringApp {
-  public static void main(String[] args) {
-    SpringApplication.run(SpringApp.class, args);
-  }
-  @Bean
-  public OpenTelemetry openTelemetry() {
-    return GlobalOpenTelemetry.get();
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
 
-  @Bean
-  public String serviceName() {
-    String serviceName = System.getProperty("otel.serviceName");
-    return   serviceName != null ? serviceName : "calendar";
-  }
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        return GlobalOpenTelemetry.get();
+    }
+
+    @Bean
+    public String serviceName() {
+        String serviceName = System.getProperty("otel.service.name");
+        return serviceName != null ? serviceName : "calendar";
+    }
 }
