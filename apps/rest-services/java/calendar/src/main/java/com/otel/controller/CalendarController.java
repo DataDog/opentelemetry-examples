22de51b24bb272d8412a278d6e1cb3c8a2881871
eugene kirillov <3404064+krlv@users.noreply.github.com>
2024-08-14 14:31:02 -0700

    Calendar app: use instrumentation annotations (#127)
    * Add a dependency on the `opentelemetry-instrumentation-annotations` library
      to use `@WithSpan` annotation.
    * Move `getDate` method to the `CalendarService` class.
    * Update OTel Java Agent to the latest `2.6.0` version
    * Fix helm instructions in the README

--- apps/rest-services/java/calendar/src/main/java/com/otel/controller/CalendarController.java
+++ apps/rest-services/java/calendar/src/main/java/com/otel/controller/CalendarController.java
@@ -1,24 +1,15 @@
 /*
 Unless explicitly stated otherwise all files in this repository are licensed
 under the Apache 2.0 License.
+
+This product includes software developed at Datadog (https://www.datadoghq.com/)
+Copyright 2024 Datadog, Inc.
 */
 package com.otel.controller;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.metrics.DoubleHistogram;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.metrics.ObservableDoubleGauge;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,62 +18,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.otel.service.CalendarService;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.metrics.DoubleHistogram;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.ObservableDoubleGauge;
+
 @RestController
 public class CalendarController {
     private final Logger log = LoggerFactory.getLogger(CalendarController.class);
-    private final Tracer tracer;
     private final LongCounter hitsCounter;
     private final DoubleHistogram latency;
     private final ObservableDoubleGauge activeUsersGauge;
     private final AtomicLong activeUsersCounter;
-    private final Random random = new Random();
+    private final CalendarService calendarService;
 
     @Autowired
-    CalendarController(OpenTelemetry openTelemetry, String serviceName) {
+    CalendarController(OpenTelemetry openTelemetry, String serviceName, CalendarService service) {
         log.info("Starting CalendarController for {}", serviceName);
-        tracer = openTelemetry.getTracer(CalendarController.class.getName());
+        
         Meter meter = openTelemetry.getMeter(CalendarController.class.getName());
         hitsCounter = meter.counterBuilder(serviceName + ".api.hits").build();
         latency = meter.histogramBuilder(serviceName + ".task.duration").build();
+        
         activeUsersCounter = new AtomicLong();
-        activeUsersGauge = meter.gaugeBuilder(serviceName + ".active.users.guage").buildWithCallback(measurement -> measurement.record(activeUsersCounter.get()));
-
+        activeUsersGauge = meter.gaugeBuilder(serviceName + ".active.users.gauge").buildWithCallback(measurement -> measurement.record(activeUsersCounter.get()));
+        
+        calendarService = service;
     }
-
 
     @GetMapping("/calendar")
     public Map<String, String> getDate(@RequestHeader MultiValueMap<String, String> headers) {
         long startTime = System.currentTimeMillis();
         activeUsersCounter.incrementAndGet();
+        
         try {
             hitsCounter.add(1);
-            String output = getDate();
-            // the correct JSON output should put this in quotes. Spring does not, so let's put quotes here
-            // by hand.
+            String output = calendarService.getDate();
             return Map.of("date", output);
         } finally {
             long endTime = System.currentTimeMillis();
             latency.record(endTime - startTime);
             activeUsersCounter.decrementAndGet();
-        }
-    }
-
-    private String getDate() {
-        Span span = tracer.spanBuilder("getDate").setAttribute("peer.service", "random-date-service").setSpanKind(SpanKind.CLIENT).startSpan();
-        try (Scope scope = span.makeCurrent()) {
-            // get back a random date in the year 2022
-            int val = new Random().nextInt(365);
-            LocalDate start = LocalDate.of(2022, Month.JANUARY, 1).plusDays(val);
-            String output = start.format(DateTimeFormatter.ISO_LOCAL_DATE);
-            span.setAttribute("date", output);
-            // Add random sleep
-            Thread.sleep(random.nextLong(1,950));
-            log.info("generated date: {}", output);
-            return output;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            span.end();
         }
     }
 }
