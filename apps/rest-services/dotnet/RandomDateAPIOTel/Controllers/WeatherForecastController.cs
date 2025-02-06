b5359976821ae124080bcae240b6fffdbeb56733
Andrei de la Cruz <andrei.delacruz@gmail.com>
2025-02-06 11:56:43 +0100

    Working otel collerated logs

--- apps/rest-services/dotnet/RandomDateAPIOTel/Controllers/WeatherForecastController.cs
+++ apps/rest-services/dotnet/RandomDateAPIOTel/Controllers/WeatherForecastController.cs
@@ -1,26 +1,72 @@
-using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Serilog.Context;
 
-namespace RandomDateAPI.Controllers
+namespace RandomDateAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RandomDateController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class RandomDateController : ControllerBase
+    private static readonly int[] value = new[] { 1, 2, 3 };
+    private readonly ILogger<RandomDateController> _logger;
+
+    public RandomDateController(ILogger<RandomDateController> logger)
     {
-        [HttpGet]
-        public DateTime Get()
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<DateTime> Get()
+    {
+        // .NET Diagnostics: create a manual span
+        using (var activity = DiagnosticsConfig.ActivitySource.StartActivity("andrei-test"))
         {
-            return GenerateRandomDate();
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+            activity?.SetTag("baz", value);
+
+            var waitTime = Random.Shared.NextDouble(); // max 1 seconds
+            await Task.Delay(TimeSpan.FromSeconds(waitTime));
+
+            activity?.SetStatus(ActivityStatusCode.Ok);
+
+            // .NET Diagnostics: update the metric
         }
 
-        private static DateTime GenerateRandomDate()
+        using (_logger.BeginScope(new Dictionary<string, object>
+               {
+                   ["dd.trace_id"] = Activity.Current.TraceId.ToString(),
+                   ["dd.span_id"] = Activity.Current.SpanId.ToString()
+               }))
         {
-            Random random = new Random();
-            int year = random.Next(1900, 2100);
-            int month = random.Next(1, 13);
-            int day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
-
-            return new DateTime(year, month, day);
+            _logger.LogCritical("Get random date from RandomDateController. {trace-id}, {span-id}",
+                Activity.Current.TraceId.ToString(), Activity.Current.SpanId.ToString());
         }
+
+        var stringTraceId = Activity.Current.TraceId.ToString();
+        var stringSpanId = Activity.Current.SpanId.ToString();
+
+        var ddTraceId = Convert.ToUInt64(stringTraceId.Substring(16), 16).ToString();
+        var ddSpanId = Convert.ToUInt64(stringSpanId, 16).ToString();
+
+        using (LogContext.PushProperty("dd.trace_id", ddTraceId))
+        using (LogContext.PushProperty("dd.span_id", ddSpanId))
+        {
+            Log.Logger.Information("Example log line with trace correlation info");
+        }
+
+        return GenerateRandomDate();
+    }
+
+    private static DateTime GenerateRandomDate()
+    {
+        var random = new Random();
+        var year = random.Next(1900, 2100);
+        var month = random.Next(1, 13);
+        var day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+
+        return new DateTime(year, month, day);
     }
 }
--- apps/rest-services/dotnet/RandomDateAPIOTel/Program.cs
+++ apps/rest-services/dotnet/RandomDateAPIOTel/Program.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using OpenTelemetry.Metrics;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +31,25 @@ builder.Services.AddOpenTelemetry()
             .AddRuntimeInstrumentation()
             .AddOtlpExporter());
 
+builder.Logging.ClearProviders();
+
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+    options.ParseStateValues = true;
+
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(DiagnosticsConfig.ServiceName))
+        .AddOtlpExporter();
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +58,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+//Add support to logging request with SERILOG
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
@@ -49,5 +74,5 @@ app.Run();
 public static class DiagnosticsConfig
 {
     public const string ServiceName = "random-date-dotnet-otel";
-    public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+    public static ActivitySource ActivitySource = new(ServiceName);
 }
