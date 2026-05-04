fface42a2be7e07172636367327948e076ac9aaa
Jade Guiton <jade.guiton@datadoghq.com>
2026-05-04 14:33:06 +0200

    Refactor generated configs into directories + minor changes

--- configurations/opentelemetry-collector/generator/configs.go
+++ configurations/opentelemetry-collector/generator/configs.go
@@ -7,10 +7,13 @@ var hostEnvs = []string{"", "ec2", "gce", "azure"}
 var k8sEnvs = []string{"", "eks"}
 
 var configs = []config{
-	{"otelcol-agent", "otelcol-agent", hostEnvs, nil},
-	{"otelcol-agent-container", "otelcol-agent", []string{""}, map[string]any{"Container": true}},
-	{"otelcol-daemonset", "otelcol-agent", k8sEnvs, map[string]any{"Container": true, "K8s": true}},
-	{"helm-daemonset", "helm-daemonset", k8sEnvs, nil},
-	{"otel-demo", "otel-demo", k8sEnvs, nil},
-	{"otel-demo-datadog", "otel-demo", []string{"eks"}, map[string]any{"DatadogExporter": true}},
+	{"agent", "otelcol-agent", hostEnvs, nil},
+	{"agent-container", "otelcol-agent", []string{""}, map[string]any{"Container": true}},
+	{"daemonset", "otelcol-agent", k8sEnvs, map[string]any{"Container": true, "K8s": true}},
+	{"helm-values/daemonset", "helm-daemonset", k8sEnvs, nil},
+
+	{"testing/agent-datadog", "otelcol-agent", []string{""}, map[string]any{"DatadogExporter": true, "Testing": true}},
+	{"testing/daemonset-datadog", "otelcol-agent", []string{""}, map[string]any{"Container": true, "K8s": true, "DatadogExporter": true, "Testing": true}},
+	{"testing/otel-demo-datadog", "otel-demo", []string{"eks"}, map[string]any{"DatadogExporter": true, "Testing": true}},
+	{"testing/otel-demo", "otel-demo", k8sEnvs, map[string]any{"Testing": true}},
 }
