ba140c4e2b041dafd1ece99d260ede30b4f85c76
Kangyi LI <kangyi.li@datadoghq.com>
2026-05-13 12:47:02 +0200

    CAP-3636 update generator to support k8s explorer (#234)
    * CAP-3636 update generator to support k8s explorer
    
    * address feedback
    
    * use OTEL_K8S_CLUSTER_NAME

--- configurations/opentelemetry-collector/generator/configs.go
+++ configurations/opentelemetry-collector/generator/configs.go
@@ -9,11 +9,14 @@ var k8sEnvs = []string{"", "eks"}
 var configs = []config{
 	{"agent", "otelcol-agent", hostEnvs, nil},
 	{"agent-container", "otelcol-agent", []string{""}, map[string]any{"Container": true}},
-	{"daemonset", "otelcol-agent", k8sEnvs, map[string]any{"Container": true, "K8s": true}},
-	{"helm-values/daemonset", "helm-daemonset", k8sEnvs, nil},
+	{"daemonset", "otelcol-agent", k8sEnvs, map[string]any{"Container": true, "K8s": true, "Kubeletstats": true}},
+	{"k8sobjects-datadog", "otelcol-k8sobjects", k8sEnvs, map[string]any{"Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
+	{"helm-values/daemonset", "helm-daemonset", k8sEnvs, map[string]any{"Kubeletstats": true}},
+	{"helm-values/k8sobjects", "helm-k8sobjects", k8sEnvs, map[string]any{"Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
 
 	{"testing/agent-datadog", "otelcol-agent", []string{""}, map[string]any{"DatadogExporter": true, "Testing": true}},
+	{"testing/helm-k8sobjects-datadog", "helm-k8sobjects", []string{"", "eks"}, map[string]any{"Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true, "Testing": true}},
 	{"testing/daemonset-datadog", "otelcol-agent", []string{""}, map[string]any{"Container": true, "K8s": true, "DatadogExporter": true, "Testing": true}},
-	{"testing/otel-demo-datadog", "otel-demo", []string{"", "eks"}, map[string]any{"DatadogExporter": true, "Testing": true, "ExperimentalRuntimeMetrics": true, "MapEquivalentMetrics": true}},
+	{"testing/otel-demo-datadog", "otel-demo", []string{"", "eks"}, map[string]any{"DatadogExporter": true, "Testing": true, "ExperimentalRuntimeMetrics": true, "MapEquivalentMetrics": true, "Kubeletstats": true}},
 	{"testing/otel-demo", "otel-demo", []string{"", "eks"}, map[string]any{"Testing": true, "ExperimentalRuntimeMetrics": true, "MapEquivalentMetrics": true}},
 }
