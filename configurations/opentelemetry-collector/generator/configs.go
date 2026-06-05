fe0478330422864e38afa0f867fd0712254c8afb
Jade Guiton <jade.guiton@datadoghq.com>
2026-06-05 15:19:25 +0200

    Add GKE (+ Autopilot) support

--- configurations/opentelemetry-collector/generator/configs.go
+++ configurations/opentelemetry-collector/generator/configs.go
@@ -4,15 +4,16 @@ var otelcolVersion = "0.152.0"
 
 var hostEnvs = []string{"", "ec2", "gce", "azure"}
 
-var k8sEnvs = []string{"", "eks"}
+var k8sDaemonsetEnvs = []string{"", "eks", "gke", "gke-autopilot"}
+var k8sDeploymentEnvs = []string{"", "eks", "gke"}
 
 var configs = []config{
 	{"agent", "otelcol-agent", hostEnvs, nil},
 	{"agent-container", "otelcol-agent", []string{""}, map[string]any{"Container": true}},
-	{"daemonset", "otelcol-agent", k8sEnvs, map[string]any{"Container": true, "K8s": true, "KubeletStats": true}},
-	{"k8s-objects-datadog", "otelcol-k8s-objects", k8sEnvs, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
-	{"helm-values/daemonset", "helm-daemonset", k8sEnvs, map[string]any{"KubeletStats": true}},
-	{"helm-values/k8s-objects-datadog", "helm-k8s-objects", k8sEnvs, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
+	{"daemonset", "otelcol-agent", k8sDaemonsetEnvs, map[string]any{"Container": true, "K8s": true, "KubeletStats": true}},
+	{"k8s-objects-datadog", "otelcol-k8s-objects", k8sDeploymentEnvs, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
+	{"helm-values/daemonset", "helm-daemonset", k8sDaemonsetEnvs, map[string]any{"KubeletStats": true}},
+	{"helm-values/k8s-objects-datadog", "helm-k8s-objects", k8sDeploymentEnvs, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true}},
 
 	{"testing/agent-datadog", "otelcol-agent", []string{""}, map[string]any{"DatadogExporter": true, "Testing": true}},
 	{"testing/helm-k8s-objects-datadog", "helm-k8s-objects", []string{"", "eks"}, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true, "Testing": true}},
