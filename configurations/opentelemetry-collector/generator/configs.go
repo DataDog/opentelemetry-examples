073a0c138fc335376837b0dba1ce68c60493ae0b
Jade Guiton <jade.guiton@datadoghq.com>
2026-06-25 19:26:32 +0200

    Add AKS (standard + automatic) configs

--- configurations/opentelemetry-collector/generator/configs.go
+++ configurations/opentelemetry-collector/generator/configs.go
@@ -4,8 +4,8 @@ var otelcolVersion = "0.152.0"
 
 var hostEnvs = []string{"", "ec2", "gce", "azure"}
 
-var k8sDaemonsetEnvs = []string{"", "eks", "gke", "gke-autopilot"}
-var k8sDeploymentEnvs = []string{"", "eks", "gke"}
+var k8sDaemonsetEnvs = []string{"", "eks", "gke", "gke-autopilot", "aks", "aks-automatic"}
+var k8sDeploymentEnvs = []string{"", "eks", "gke", "aks"}
 
 var configs = []config{
 	{"agent", "otelcol-agent", hostEnvs, nil},
@@ -17,7 +17,7 @@ var configs = []config{
 
 	{"testing/agent-datadog", "otelcol-agent", []string{""}, map[string]any{"DatadogExporter": true, "Testing": true}},
 	{"testing/helm-k8s-objects-datadog", "helm-k8s-objects", []string{"", "eks"}, map[string]any{"Container": true, "K8s": true, "Deployment": true, "DatadogExporter": true, "KSM": true, "K8sObjects": true, "Testing": true}},
-	{"testing/daemonset-datadog", "otelcol-agent", []string{""}, map[string]any{"Container": true, "K8s": true, "DatadogExporter": true, "Testing": true}},
+	{"testing/daemonset-datadog", "otelcol-agent", []string{"", "aks-automatic"}, map[string]any{"Container": true, "K8s": true, "DatadogExporter": true, "Testing": true}},
 	{"testing/otel-demo-datadog", "otel-demo", []string{"", "eks"}, map[string]any{"DatadogExporter": true, "Testing": true, "ExperimentalRuntimeMetrics": true, "MapEquivalentMetrics": true, "KubeletStats": true}},
 	{"testing/otel-demo", "otel-demo", []string{"", "eks"}, map[string]any{"Testing": true, "ExperimentalRuntimeMetrics": true, "MapEquivalentMetrics": true, "DualShip": true}},
 }
